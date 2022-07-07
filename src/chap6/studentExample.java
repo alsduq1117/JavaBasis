3593ece65591a8012e67558465126eb90d1d2789
Minyeob Kim <alsduq1117@naver.com>
2022-07-08 04:33:25 +0900

    챕터6 커밋

---
+++ src/chap6/studentExample.java
@@ -0,0 +1,12 @@
+package chap6;
+
+public class studentExample {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        System.out.println("s1 변수가 Student 객체를 참조합니다.");
+
+        Student s2 = new Student();
+        System.out.println("s2 뱐스기 또 다른 Student 객체를 참조합니다.");
+    }
+
+}
